677cd3808df836e654bc6ccf5376f421d4a6881d
KartelAG <masterche1@mail.ru>
2019-09-04 17:28:56 +0500

    Add logging to a file for every thread

--- multiproc.py
+++ multiproc.py
@@ -3,10 +3,17 @@
 import sys
 import subprocess
 
+threads = 40
+
 procs = []
-for i in range(20):
-    proc = subprocess.Popen([sys.executable, 'pyapp.py'])
+logfile = []
+for i in range(threads):
+    logfile.insert(i, open("logs/log"+str(i)+".txt", 'w'))
+    proc = subprocess.Popen([sys.executable, 'pyapp.py'], stdout=logfile[i])
     procs.append(proc)
 
 for proc in procs:
     proc.wait()
+
+for i in range(threads):
+    logfile[i].close()
